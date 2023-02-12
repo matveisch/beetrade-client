0f06f38abff777d527d656a389373b88024fe0fd
Matvei <matvesch@protonmail.com>
2023-02-12 14:54:38 +0200

    tons of stuff – sign out, pop up, header

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 import classes from './Navbar.module.scss';
 import Profile from './Profile/Profile';
 import CourseChooserNavbar from './CourseChooserNavbar/CourseChooserNavbar';
+import { useAppSelector } from '../../hooks';
+import { selectUserSession } from '../../features/userSession/userSessionSlice';
 
 function Navbar() {
+  const token = useAppSelector(selectUserSession);
+
   return (
-    <div className={classes.navbar}>
-      <Profile />
-      <CourseChooserNavbar />
+    <div
+      className={classes.navbar}
+      style={!token ? { gridTemplateColumns: 'unset', justifyContent: 'end' } : undefined}>
+      {token && <Profile />}
+      {token && <CourseChooserNavbar />}
       <h1>BeeTrade</h1>
     </div>
   );
