89b4769eb2740824eec1256af56323cc2bb16feb
Matvei <matvesch@protonmail.com>
2023-02-12 11:05:56 +0200

    basic navbar grid

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,12 @@
 import classes from './Navbar.module.scss';
+import Profile from './Profile/Profile';
+import CourseChooserNavbar from './CourseChooserNavbar/CourseChooserNavbar';
 
 function Navbar() {
   return (
     <div className={classes.navbar}>
+      <Profile />
+      <CourseChooserNavbar />
       <h1>BeeTrade</h1>
     </div>
   );
