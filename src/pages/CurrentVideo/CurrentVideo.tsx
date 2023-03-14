6df5b0159cd087d6cd02a27793c5cba6aa6316a8
Matvei <matvesch@protonmail.com>
2023-03-14 15:47:48 +0200

    tablet version

--- src/pages/CurrentVideo/CurrentVideo.tsx
+++ src/pages/CurrentVideo/CurrentVideo.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import classes from './CurrentVideo.module.scss';
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
 import Sidebar from '../../components/Sidebar/Sidebar';
@@ -18,11 +20,23 @@ function CurrentVideo() {
   const videos = useAppSelector(selectVideos);
   const currentVideo = useAppSelector(selectCurrentVideo);
   const currentSection = useAppSelector(selectCurrentSection);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [currentView, setCurrentView] = useState<'contents' | 'sidebar'>('contents');
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  });
 
   // todo: error handling
   if (!videos || !currentVideo || !currentSection) return <div>error</div>;
 
-  return (
+  return windowWidth > 768 ? (
     <div className={classes.currentVideo}>
       <div className={classes.videoPlayerContainer}>
         <VideoPlayer />
@@ -32,6 +46,32 @@ function CurrentVideo() {
         currentSectionVideos={videos.filter(video => video.section._id === currentSection._id)}
         videos={videos}
       />
+    </div>
+  ) : (
+    <div className={classes.currentVideoMobile}>
+      <VideoPlayer />
+      <div className={classes.swiper}>
+        <div className={classes.buttons}>
+          <button type="button" onClick={() => setCurrentView('contents')}>
+            Contents
+            <div
+              className={classes.afterElement}
+              style={currentView !== 'contents' ? { display: 'none' } : undefined}
+            />
+          </button>
+          <button type="button" onClick={() => setCurrentView('sidebar')}>
+            Sidebar
+            <div className={classes.afterElement} style={currentView !== 'sidebar' ? { display: 'none' } : undefined} />
+          </button>
+        </div>
+      </div>
+      {currentView === 'contents' && <Contents />}
+      {currentView === 'sidebar' && (
+        <Sidebar
+          currentSectionVideos={videos.filter(video => video.section._id === currentSection._id)}
+          videos={videos}
+        />
+      )}
     </div>
   );
 }
