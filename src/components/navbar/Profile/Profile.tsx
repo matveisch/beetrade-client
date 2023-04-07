4ab964c339dd579a47e4debdbfd5045a0ad392b7
Matvei <matvesch@protonmail.com>
2023-04-07 18:53:53 +0300

    click outside of profile popup

--- src/components/navbar/Profile/Profile.tsx
+++ src/components/navbar/Profile/Profile.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import classes from './Profile.module.scss';
 import profileIcon from '../../../assets/images/profileImage.svg';
 import ProfilePopup from './ProfilePopup/ProfilePopup';
 
 function Profile() {
   const [openPopup, setOpenPopup] = useState(false);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  function handleClickOutside(e: { target: any }) {
+    if (wrapperRef.current && !wrapperRef.current.contains(e.target)) {
+      setOpenPopup(false);
+    }
+  }
 
   return (
     <div className={classes.profile}>
@@ -12,7 +19,11 @@ function Profile() {
         <h1>פרופיל</h1>
         <img src={profileIcon} alt="profile-icon" />
       </button>
-      <ProfilePopup open={openPopup} setOpen={setOpenPopup} />
+      <div ref={wrapperRef}>
+        <ProfilePopup open={openPopup} setOpen={setOpenPopup} />
+      </div>
+      {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+      {openPopup && <div className={classes.popupWrapper} onClick={handleClickOutside} />}
     </div>
   );
 }
