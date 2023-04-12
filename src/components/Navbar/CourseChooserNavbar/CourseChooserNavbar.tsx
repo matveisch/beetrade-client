61fd6328cf9849c43641a141b01e7ce07b4c948c
Matvei <matvesch@protonmail.com>
2023-04-12 17:49:56 +0300

    course chooser basic fixes

--- src/components/Navbar/CourseChooserNavbar/CourseChooserNavbar.tsx
+++ src/components/Navbar/CourseChooserNavbar/CourseChooserNavbar.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import classes from './CourseChooserNavbar.module.scss';
 import arrowDown from '../../../assets/images/arrowDown.svg';
+import { useAppSelector } from '../../../hooks';
+import { selectCurrentVideo } from '../../../features/currentVideo/currentVideoSlice';
+import { selectUserData } from '../../../features/userData/userDataSlice';
 
 function CourseChooserNavbar() {
+  const [popupOpen, setPopupOpen] = useState(false);
+  const currentVideo = useAppSelector(selectCurrentVideo);
+  const userData = useAppSelector(selectUserData);
+
   return (
-    <div className={classes.courseChooserNavbar}>
-      <h1>קורס: מ-0 עד דבש</h1>
-      <img src={arrowDown} alt="arrow-down" />
+    <div className={classes.chooserWrapper}>
+      <div className={classes.courseChooserNavbar} onClick={() => setPopupOpen(!popupOpen)}>
+        <h1>{currentVideo?.course.name}</h1>
+        <img src={arrowDown} alt="arrow-down" style={popupOpen ? { transform: 'rotate(180deg)' } : undefined} />
+      </div>
+      {popupOpen && (
+        <div className={classes.courseChooserPopup}>
+          <div className={classes.innerCourseChooserPopup}>
+            {userData?.courses.map(course => {
+              return (
+                <p key={course.name} onClick={() => setPopupOpen(false)}>
+                  {course.name}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
