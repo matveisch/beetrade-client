11db0749dca0c6d64762edbb5b30d263595d8ca2
Matvei <matvesch@protonmail.com>
2023-04-26 20:48:34 +0300

    popup scroll lock

--- src/components/Navbar/Profile/Profile.tsx
+++ src/components/Navbar/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import classes from './Profile.module.scss';
 import profileIcon from '../../../assets/images/profileImage.svg';
 import ProfilePopup from './ProfilePopup/ProfilePopup';
@@ -6,12 +6,34 @@ import ProfilePopup from './ProfilePopup/ProfilePopup';
 function Profile() {
   const [openPopup, setOpenPopup] = useState(false);
   const wrapperRef = useRef<HTMLDivElement>(null);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   function handleClickOutside(e: { target: any }) {
     if (wrapperRef.current && !wrapperRef.current.contains(e.target)) {
       setOpenPopup(false);
     }
   }
+
+  useEffect(() => {
+    if (openPopup && screenWidth < 425) {
+      document.body.style.overflow = 'hidden';
+      return () => {
+        document.body.style.overflow = 'unset';
+      };
+    }
+  }, [openPopup, screenWidth]);
 
   return (
     <div className={classes.profile}>
@@ -23,7 +45,7 @@ function Profile() {
         <ProfilePopup open={openPopup} setOpen={setOpenPopup} />
       </div>
       {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
-      {openPopup && <div className={classes.popupWrapper} onClick={handleClickOutside} />}
+      {openPopup && screenWidth > 425 && <div className={classes.popupWrapper} onClick={handleClickOutside} />}
     </div>
   );
 }
